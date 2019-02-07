0e498438e8e98383a9e43e6afe7d9d1d375b9be2
Sharmarke Duale <sduale01@gmail.com>
2019-02-07 15:34:31 -0600

    add form inputs

--- src/components/AddToShelf/AddToShelf.js
+++ src/components/AddToShelf/AddToShelf.js
@@ -5,18 +5,44 @@ class AddToShelf extends Component {
         super();
 
         this.state = {
-            shelfItem: {
-                description: '',
-                image_url: '',
-                person_id: 0
-            }
+            description: '',
+            image_url: '',
         }
+    }
+
+    // capture the description
+    handleDescriptionChange = (event) => {
+        this.setState({
+            ...this.state,
+            description: event.target.value,
+        });
+        console.log(this.state.description);
+        
+    }
+
+    // capture the image url
+    handleImageChange = (event) => {
+        this.setState({
+            ...this.state,
+            image_url: event.target.value,
+        });
+        console.log(this.state.image_url);
+        
+    }
+
+    handleBackClick = () => {
+        this.props.history.push('/shelf')
     }
     render(){
         return (
             <div>
                 <h3>This page will add to the shelf</h3>
-
+                {JSON.stringify(this.state)}
+                <form onSubmit={this.handleSubmit}>
+                    <input onChange={this.handleDescriptionChange} type="text" placeholder="description"/>
+                    <input onchange={this.handleImageChange} type="text" placeholder="image url"/>
+                </form>
+                <button onClick={this.handleBackClick}>BACK</button>
             </div>
         );
     }
